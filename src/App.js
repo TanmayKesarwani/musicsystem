85f826618d24e22e50bed9991a91f164c6df4ce7
Tanmay Kesarwani <tanmay.k@valuebound.com>
2021-09-27 11:04:15 +0530

    musicsystem2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './main.css';
+import data from './data';
+
+import MusicList from './music';
+
 
 function App() {
+  const[search, setSearch] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <div className='title'>
+          <h3 className='music_system'>Music System</h3>
+        </div>
+        <button className="signout">
+          signout
+        </button>
       </header>
+      <section>
+        <section>
+          <div className="search">
+            <input type="text" placeholder='Search All Tracks' onChange={(val) => {setSearch(val.target.value)}} />
+          </div>
+        </section>
+        
+        {
+          data.filter((val) => { 
+            //if string is empty, display all the music
+            if(search === '') {
+              return val
+            }
+            //if not then display the matched music
+            else if(val.title.toLowerCase().includes(search.toLowerCase())) {
+              return val
+            }
+          }).map((val) => {
+            return <MusicList key={val.id} {...val}> </MusicList>
+          })
+          
+        }
+      </section>
     </div>
   );
+  
 }
 
 export default App;
--- src/music.js
+++ src/music.js
@@ -1,10 +1,9 @@
-// import React, { useState } from 'react';
+ // import React, { useState } from 'react';
 import data from './data';
 import { useState } from "react";
 
 
 const MusicList = (props) => {
-    console.log("jdbbc");
     const{id,like,title,subtitle,media} = props;
 
     const[visible,setVisible] = useState(false);
@@ -12,14 +11,13 @@ const MusicList = (props) => {
     const[music,setMusic] = useState(data);
 
     const RemoveItem = (id) => {
-        let newMusic = music.filter((music) => music.id !== id);
-        setMusic(newMusic);
+        setMusic(music.filter((musics) => musics.id !== id));
     }
 
     return (
         <>
         <article>
-            <div>
+            <div className='musicField'>
                 <label className='like-text'>{totalLike}</label>
                 <button className="like-button" onClick={() => {setTotalLike(totalLike + 1)}}></button>
                 <div className="song">
@@ -28,8 +26,8 @@ const MusicList = (props) => {
                     <button className="play">Play</button>
                     <button className="arrow" onClick={() => {setVisible(!visible)}}></button>
                 </div>
-                if(visible) {
-                    <div className="delete">
+                {
+                    visible && <div className="delete">
                         <button className="delete" onClick={() => RemoveItem(id)}>Delete Song</button>
                     </div>
                 }
